0d70bc931bd4f52e926246844684b33514eda2d0
tanzilrayhan <tanzilrayhan169@gmail.com>
2023-09-02 03:02:59 +0600

    verified icon

--- index.js
+++ index.js
@@ -4,12 +4,13 @@ const functionCategory = async () => {
     const response = await fetch("https://openapi.programming-hero.com/api/videos/categories");
     const data = await response.json();
 
-    const tabContainer = document.getElementById("tab-container")
+    const tabContainer = document.getElementById("tab-container");
     data.data.forEach((category) => {
         const div = document.createElement("div");
         div.innerHTML = ` <a onclick="loadData('${category.category_id}')" class="btn btn-active  px-5">${category.category}</a> `;
         tabContainer.append(div)
     });
+
 }
 
 const loadData = async (id) => {
@@ -21,26 +22,6 @@ const loadData = async (id) => {
     const cardContainer = document.getElementById("card-container")
     cardContainer.innerHTML = "";
 
-    if(data.data.length === 0){
-        const noContent = document.getElementById("no-content");
-        noContent.innerHTML = "";
-        const div = document.createElement("div");
-        div.innerHTML = `
-        <div class="flex items-center justify-center flex-col mt-20 ">
-        <img src="./Images/Icon.png">
-        <h1 class="font-bold text-3xl text-center mt-4">
-            Oops!! Sorry, There is no <br> content here
-        </h1>
-        </div>
-        `;
-        noContent.appendChild(div);
-    }
-    else{
-        // const noContent = document.getElementById("no-content").classList.add('hidden');
-        // noContent.innerHTML = "";
-        
-    }
-
     data.data?.forEach((videos) => {
         const div = document.createElement("div");
         div.innerHTML = ` 
@@ -48,7 +29,7 @@ const loadData = async (id) => {
         <figure>
             <div class="relative">
             <img class="rounded-md h-52 w-80" src=${videos.thumbnail}/>
-            <span class="w-24 text-xs text-white rounded-md p-1 absolute bottom-0 right-12 ">${videos.others?.posted_date}</span>
+            <span class="w-24 text-xs text-white rounded-md p-1 absolute bottom-0 right-12 ">${videos.others.posted_date}</span>
         </div>
         </figure>
         <div class="flex gap-5 mt-3">
@@ -64,7 +45,7 @@ const loadData = async (id) => {
                 <div>
                 <h3 class="flex items-center gap-1 my-1">
                 ${videos.authors[0].profile_name}
-                <div id="badge"><img src="./images/verified.png">${videos.authors[0].verified ? videos.authors[0].verified : ""}</div>
+                <div id="badge"><img src=${videos.authors[0].verified  ? "./images/verified.png" : " "  }></div>
                     </h3>
             <p>${videos.others.views} views</p>
             </div>
@@ -74,16 +55,53 @@ const loadData = async (id) => {
       </div>
         `;
         cardContainer.append(div);
+
+        // const time = ${videos.others.posted_date}
+        // const totalSeconds = time;
+
+        // let hours = Math.floor(seconds/3600);
+        // let minutes = Math.floor(seconds - (hours*3600)/60);
+        // const displayTime = time ? ` ${hours} hrs ${minutes} min ago` : "";
+
+        // const sortView = data.data;
+
+        const sortView = ((a,b) => b.others.views - a.others.views);
+
+
+
     })
+
+    const noContent = document.getElementById("no-content");
+    noContent.innerHTML = "";
+
+    if(data.data.length === 0){
+        const div = document.createElement("div");
+        div.innerHTML = `
+        <div class="flex items-center justify-center flex-col mt-20 ">
+        <img src="./Images/Icon.png">
+        <h1 class="font-bold text-3xl text-center mt-4">
+            Oops!! Sorry, There is no <br> content here
+        </h1>
+        </div>
+        `;
+        noContent.appendChild(div);
+    }
+    else{}
     
 }
 
 const timeConvert = async (seconds) => {
     let hours = Math.floor(seconds/3600);
     let minutes = Math.floor(seconds - (hours*3600)/60);
-
-    return hours + "hrs " + minutes + "min " + "ago";
+    return ` ${hours} hrs ${minutes} min ago`;
 }
+
+        // const time = videos.others?.posted_date;
+        // const totalSeconds = time;
+
+        // let hours = Math.floor(seconds/3600);
+        // let minutes = Math.floor(seconds - (hours*3600)/60);
+        // const displayTime = time ? ` ${hours} hrs ${minutes} min ago` : "";
 
 functionCategory();
 loadData("1000");
